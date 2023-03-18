d6223032ee490f08b7a5c5d23b4b68be3e495305
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-18 13:39:08 +0200

    - Create an instance of the `Bot`

--- source/botty/app.d
+++ source/botty/app.d
@@ -2,8 +2,13 @@ module botty.app;
 
 import std.stdio;
 import birchwood;
+import botty.bot : Bot;
 
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
+
+	ConnectionInfo connInfo;
+
+	Bot botty = new Bot(connInfo);
 }
