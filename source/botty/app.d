6ab500c2cbaa147eeacc5f32214616e75e9a8231
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-06 07:50:45 +0200

    App
    - Call `start()` on the `Bot` after creation
    - Added a connection test to `worcester.bnet` for testing

--- source/botty/app.d
+++ source/botty/app.d
@@ -8,7 +8,13 @@ void main()
 {
 	writeln("Edit source/app.d to start your project.");
 
-	ConnectionInfo connInfo;
+	ConnectionInfo connInfo = ConnectionInfo.newConnection("worcester.community.networks.deavmi.assigned.network", 6667, "birchwood", "doggie", "Tristan B. Kildaire");
 
+	// // Set the fakelag to 1 second
+	// connInfo.setFakeLag(1);
+	
 	Bot botty = new Bot(connInfo);
+
+	// Start the bot
+	botty.start();
 }
