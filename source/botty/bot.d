7d17def84f7fd67ea3b13b03571c57b703a6a26b
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-06 07:49:47 +0200

    Bot
    - Added a `start()` method which will call `connect()` from the birchwood `Client` inherited member

--- source/botty/bot.d
+++ source/botty/bot.d
@@ -1,12 +1,23 @@
 module botty.bot;
 
 import birchwood;
+import lumars;
 
 public class Bot : Client
 {
     this(ConnectionInfo info)
     {
         super(info);
+    }
+
+    /** 
+     * Starts the bot by initializing the modules
+     * and connecting it to the remote server
+     */
+    public void start()
+    {
+        // Authenticate
+    	connect();
     }
 
     public override void onChannelMessage(Message fullMessage, string channel, string msgBody)
