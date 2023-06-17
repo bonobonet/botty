ae4a21326b706451dcd397446382ec876a0ea921
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-17 12:28:43 +0200

    App
    - Added configuration file parsing

--- source/botty/app.d
+++ source/botty/app.d
@@ -3,9 +3,32 @@ module botty.app;
 import std.stdio;
 import birchwood;
 import botty.bot : Bot;
+import std.exception : ErrnoException;
+import std.json : parseJSON, JSONValue, JSONException;
+import core.stdc.stdlib : exit;
 
 void main()
 {
+	string configPath = "config.json";
+	Config config;
+
+	try
+	{
+		config = getConfig(configPath);
+	}
+	catch(ErrnoException e)
+	{
+		// TODO: Replace all writeln with gogga logs
+		writeln("Could not open configuration file '"~configPath~"'");
+		exit(-1);
+	}
+	catch(JSONException e)
+	{
+		writeln("Error parsing the JSON in config file '"~configPath~"'");
+		exit(-1);
+	}
+
+	// TODO: Extract config info here and set it in the `connInfo` below
 	ConnectionInfo connInfo = ConnectionInfo.newConnection("worcester.community.networks.deavmi.assigned.network", 6667, "bottyng", "doggie", "Tristan B. Kildaire");
 
 	// // Set the fakelag to 1 second
@@ -15,4 +38,28 @@ void main()
 
 	// Start the bot
 	botty.start();
+}
+
+struct Config
+{
+	string serverAddr;
+	int serverPort;
+}
+
+Config getConfig(string configPath)
+{
+	File configFile;
+	configFile.open(configPath);
+
+	ubyte[] data;
+	data.length = configFile.size();
+	configFile.rawRead(data);
+	configFile.close();
+
+	import jstruct;
+
+	JSONValue json = parseJSON(cast(string)data);
+	Config config = fromJSON!(Config)(json);
+
+	return config;
 }
