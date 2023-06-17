98f3cb0e328e7033f98e33218e7ca2c9ec5f4a6d
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-17 14:00:39 +0200

    Mod
    - Defined the structure of a module for the botty
    - This includes an `accepts()` method which determine if the routine should be called
    - it also includes the routine itself in the form of a `react()` method
    - There is a stored `Bot` instance accessible from the super class that can be used in order to send messages to a channel and general bot-related tasks (birchwood tasks too)

---
+++ source/botty/mod.d
@@ -0,0 +1,25 @@
+module botty.mod;
+
+import botty.bot : Bot;
+import birchwood.protocol.messages : Message;
+
+/** 
+ * Represents a module that has code that can be run
+ * dependent on whether it accepts the provided message
+ */
+public abstract class Mod
+{
+    private Bot bot;
+    this(Bot bot)
+    {
+        this.bot = bot;
+    }
+
+    public final Bot getBot()
+    {
+        return bot;
+    }
+
+    public abstract bool accepts(Message fullMessage, string channel, string messageBody);
+    public abstract void react(Message fullMessage, string channel, string messageBody);
+}
