2ce8d58246efec4c9cc225f8f7d6110230db047d
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-17 16:10:29 +0200

    Ping
    - Added stub class
    - Implemented working `accepts()` method
    - Added stub  `react()` method

--- source/botty/modules/ping.d
+++ source/botty/modules/ping.d
@@ -3,3 +3,29 @@
  */
 module botty.modules.ping;
 
+module botty.modules.deavmicomedy;
+
+import botty.mod;
+import botty.bot : Bot;
+import birchwood.protocol.messages : Message;
+
+public class Ping : Mod
+{
+    private static string commandStr = ".ping";
+
+    this(Bot bot)
+    {
+        super(bot);
+    }
+
+    public override bool accepts(Message fullMessage, string channel, string messageBody)
+    {
+        import std.string : startsWith;
+        return messageBody.startsWith(commandStr);
+    }
+
+    public override void react(Message fullMessage, string channel, string messageBody)
+    {
+        // TODO: Implement me
+    }
+}
