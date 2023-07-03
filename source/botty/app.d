ebe50679b7388006ec11903532037c24e0f137b5
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-07-03 14:41:24 +0200

    App
    - Set fake lag to `0`

--- source/botty/app.d
+++ source/botty/app.d
@@ -36,7 +36,7 @@ void main(string[] args)
 	ConnectionInfo connInfo = ConnectionInfo.newConnection(config.serverAddr, cast(ushort)config.serverPort, "bottyng", "doggie", "Tristan B. Kildaire");
 
 	// // Set the fakelag to 1 second
-	// connInfo.setFakeLag(1);
+	connInfo.setFakeLag(0);
 
 
 	Bot botty = new Bot(connInfo, config);
